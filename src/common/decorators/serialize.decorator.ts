c5b11d26f533c704f34498d426587dc1c7c273c7
GhZzj <17074140@qq.com>
2025-06-12 11:55:38 +0800

    style(decorators): 修复序列化装饰器的格式问题
    调整Serialize装饰器的代码格式，包括添加空格和修复文件末尾的换行符问题，以提高代码可读性

--- src/common/decorators/serialize.decorator.ts
+++ src/common/decorators/serialize.decorator.ts
@@ -2,6 +2,6 @@ import { UseInterceptors } from "@nestjs/common"
 import { SerializerInterceptor } from "../interceptors/serialize.inceptor"
 
 //创建一个serialize装饰器
-export const Serialize=(dto:any,flag?:boolean)=>{
+export const Serialize= (dto:any,flag?:boolean)=>{
     return UseInterceptors(new SerializerInterceptor(dto,flag))
 }
